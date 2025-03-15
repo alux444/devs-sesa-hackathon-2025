0e9bbbb3600d3ecf33c283fb11ffadbb33160c50
Alex Liang <126375355+alux444@users.noreply.github.com>
2025-03-15 19:42:41 +1300

    Moved counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Pokemon from './components/Pokemon'
-import Cats from './components/Cats'
-import DeckOfCards from './components/DeckOfCards'
+import { useState } from "react";
+import "./App.css";
+import Pokemon from "./components/Pokemon";
+import Cats from "./components/Cats";
+import DeckOfCards from "./components/DeckOfCards";
+import CounterExample from "./components/CounterExample";
 
 function App() {
-  const [count, setCount] = useState(0)
   const [currentPage, setCurrentPage] = useState("cats");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>
+        <span className="gradientTextPurple">DEVS</span> 
+        {" x "}
+        <span className="gradientTextGreen">SESA</span>
+        {" "}Hackathon 2025
+      </h1>
 
-      <button onClick={() => setCurrentPage("cats")}>Cats</button>
-      <button onClick={() => setCurrentPage("pokemon")}>Pokemon</button>
-      <button onClick={() => setCurrentPage("deckOfCards")}>Deck of Cards</button>
+      <div className="flexGapStyle">
+        <button onClick={() => setCurrentPage("cats")}>Cats</button>
+        <button onClick={() => setCurrentPage("deckOfCards")}>Deck of Cards</button>
+        <button onClick={() => setCurrentPage("pokemon")}>Pokemon</button>
+        <button onClick={() => setCurrentPage("counterExample")}>Counter Example</button>
+      </div>
 
       <h2>Current page: {currentPage}</h2>
 
       {currentPage === "cats" && <Cats />}
-      {currentPage === "pokemon" && <Pokemon />}
       {currentPage === "deckOfCards" && <DeckOfCards />}
+      {currentPage === "pokemon" && <Pokemon />}
+      {currentPage === "counterExample" && <CounterExample />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
